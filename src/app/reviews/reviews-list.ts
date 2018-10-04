c7f5a67770ee7efb2473bc59c2891a0dff0cae64
wydra76yan <yan@vydra.com>
2018-10-04 11:55:08 +0300

    Russian language and reviews info updated

--- src/app/reviews/reviews-list.ts
+++ src/app/reviews/reviews-list.ts
@@ -11,13 +11,13 @@ export const ReviewsList: ReviewsModel[] = [
         reviewsList: [
           {
             id: 0,
-            description: 'Best ever',
+            description: 'Как студент, могу сказать следующее. Уровень образования и пед. состав - мягко скажем не очень.',
             date: '01/01/1998',
-            author: 'Bunny',
+            author: 'anihilator',
           },
           {
             id: 1,
-            description: 'w Best not ever',
+            description: 'Mar oileanach, urrainn dhomh na leanas a ràdh. Ìre foghlaim agus ped. Sgrìobhadh - airson a chuir gu math, chan ann gu mòr.',
             date: '01/01/1998',
             author: 'Bunny',
           }
@@ -29,13 +29,13 @@ export const ReviewsList: ReviewsModel[] = [
         reviewsList: [
           {
            id: 0,
-           description: 'y Norm',
+           description: 'Ikasle gisa, honako hau esan dezaket. Heziketa maila eta ped. Konposizioa - apur bat jartzea, ez oso.',
            date: '01/01/1998',
            author: 'Ghy',
           },
           {
             id: 1,
-            description: 'd Not Norm',
+            description: 'As a student, I can say the following. Level of education and ped. Composition - to put it mildly, not very much.',
             date: '01/01/1998',
             author: 'Ghy',
            }
@@ -46,13 +46,13 @@ export const ReviewsList: ReviewsModel[] = [
         universityName: 'Факультет Педагогики и Искусств',
         reviewsList: [{
             id: 0,
-            description: 'r Best ever',
+            description: 'ווי אַ תּלמיד, איך קענען זאָגן די פאלגענדע. גלייַך פון בילדונג און פּעד. זאַץ - צו לייגן עס מיילדלי, נישט זייער פיל.',
             date: '01/01/1998',
             author: 'Bunny',
           },
           {
             id: 1,
-            description: 'a Not Best ever',
+            description: 'Diákként elmondhatom a következőket. Az oktatás színvonala és ped. Összetétel - enyhén szólva',
             date: '01/01/1998',
             author: 'Bunny',
           }
@@ -63,7 +63,7 @@ export const ReviewsList: ReviewsModel[] = [
         universityName: 'Поморский Университет в Слупске',
         reviewsList: [{
             id: 0,
-            description: 'p kek cheburek',
+            description: 'Като студент мога да кажа следното. Ниво на образование и преподаване. Композиция - да я сложиш леко, не много',
             date: '01/01/1998',
             author: 'Ghy',
           }
@@ -80,7 +80,7 @@ export const ReviewsList: ReviewsModel[] = [
         universityName: 'Каунасский Технологический Колледж',
         reviewsList: [{
             id: 0,
-            description: 'i Norm',
+            description: 'Là một sinh viên, tôi có thể nói như sau. Trình độ học vấn và giáo dục. Thành phần - để đặt nó nhẹ nhàng, không phải là rất nhiều.',
             date: '01/01/1998',
             author: 'Pani',
           }
@@ -88,10 +88,10 @@ export const ReviewsList: ReviewsModel[] = [
       },
       {
         id: 0,
-        universityName: 'Университет Миколаса Рёмериса ',
+        universityName: 'Университет Миколаса Рёмериса',
         reviewsList: [{
             id: 0,
-            description: 'd Best ever',
+            description: 'Mar oileanach, urrainn dhomh na leanas a ràdh. Ìre foghlaim agus ped. Sgrìobhadh - airson a chuir gu math, chan ann gu mòr.',
             date: '01/05/1968',
             author: 'Bunny',
           }
@@ -102,7 +102,7 @@ export const ReviewsList: ReviewsModel[] = [
         universityName: 'Университет Клайпеды',
         reviewsList: [{
             id: 0,
-            description: 'o Lol',
+            description: 'ווי אַ תּלמיד, איך קענען זאָגן די פאלגענדע. גלייַך פון בילדונג און פּעד. זאַץ - צו לייגן עס מיילדלי, נישט זייער פיל.',
             date: '01/01/1998',
             author: 'Pani',
           }
@@ -111,13 +111,107 @@ export const ReviewsList: ReviewsModel[] = [
       {
         id: 0,
         universityName: 'Университет в Шауляй',
-        reviewsList: [
-          {
+        reviewsList: [{
             id: 0,
-            description: 'r Norm',
+            description: 'Като студент мога да кажа следното. Ниво на образование и преподаване. Композиция - да я сложиш леко, не много',
             date: '01/01/1998',
             author: 'Pani',
           }
+        ]
+      },
+    ]
+  },
+  {
+    id: 0,
+    countryName: 'Чехия',
+    universityList: [
+      {
+        id: 0,
+        universityName: 'Университет Палацкого',
+        reviewsList: [{
+            id: 0,
+            description: 'As a student, I can say the following. Level of education and ped. Composition - to put it mildly, not very much.',
+            date: '01/01/1998',
+            author: 'Pani',
+          }
+        ]
+      },
+      {
+        id: 0,
+        universityName: 'Университет в Пардубице',
+        reviewsList: [{
+          id: 0,
+          description: 'Как студент, могу сказать следующее. Уровень образования и пед. состав - мягко скажем не очень.',
+          date: '01/05/1968',
+          author: 'Bunny',
+        }
+        ]
+      },
+    ]
+  },
+  {
+    id: 0,
+    countryName: 'Румыния',
+    universityList: [
+      {
+        id: 0,
+        universityName: 'Университет Питешти',
+        reviewsList: [{
+          id: 0,
+          description: 'Sicut discipulus, non possum dicere sequentibus. Level educationis ped. structure - ut levissime dicam, non est valde.\n',
+          date: '01/01/1998',
+          author: 'Pani',
+        }
+        ]
+      },
+      {
+        id: 0,
+        universityName: 'Университет имени Александра Иона Кузы в Яссы',
+        reviewsList: [{
+          id: 0,
+          description: 'Sicut discipulus, non possum dicere sequentibus. Level educationis ped.',
+          date: '01/05/1968',
+          author: 'Bunny',
+        }
+        ]
+      },
+      {
+        id: 0,
+        universityName: 'Университет ветеринариной медицины имени Короля Михала Первого',
+        reviewsList: [{
+          id: 0,
+          description: 'Як студэнт, магу сказаць наступнае. Ўзровень адукацыі і пед. склад - мякка скажам не вельмі.',
+          date: '01/05/1968',
+          author: 'Bunny',
+        }
+        ]
+      },
+    ]
+  },
+  {
+    id: 0,
+    countryName: 'Испания',
+    universityList: [
+      {
+        id: 0,
+        universityName: 'Университет Гранады',
+        reviewsList: [{
+          id: 0,
+          description: 'Là một sinh viên, tôi có thể nói như sau. Trình độ học vấn và giáo dục. Thành phần - để đặt nó nhẹ nhàng',
+          date: '01/01/1998',
+          author: 'Pani',
+        }
+        ]
+      },
+      {
+        id: 0,
+        universityName: 'Университет Вальядолида',
+        reviewsList: [{
+          id: 0,
+          description: 'As studint kin ik de folgjende sizze. Nivo fan oplieding en ped. Komposysje - it leuk, net folle.',
+          date: '01/05/1968',
+          author: 'Bunny',
+        }
         ]
       },
     ]
